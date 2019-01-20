346418a066e5ab09c245af8159fb6e1a9e1cec18
Ochanissi <mirelalex2094@gmail.com>
2019-01-20 21:26:02 +0200

    fixing some bugs

--- src/CardList.js
+++ src/CardList.js
@@ -5,18 +5,18 @@ const CardList = ({ robots }) => {
 
     return (
         <div>
-        {
-        robots.map((user, i) => {
-            return  (  
-                <Card       
-                key={i} 
-                id={robots[i].id} 
-                name={robots[i].name} 
-                email={robots[i].email} 
-                />
-        );
-    })
-    }
+         {
+                robots.map((user, i) => {
+                    return  (  
+                        <Card       
+                        key={i} 
+                        id={robots[i].id} 
+                        name={robots[i].name} 
+                        email={robots[i].email} 
+                        />
+                );
+            })
+        }
 
     </div>
 
