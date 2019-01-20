02e2991fd9411ffc4934d19055b4942e5dafc9d0
Ochanissi <mirelalex2094@gmail.com>
2019-01-20 21:23:41 +0200

    Making the application dynamic

--- src/Card.js
+++ src/Card.js
@@ -2,16 +2,14 @@ import React from 'react';
 
 const Card = ({ name, email, id }) => {
     return (
-        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <h1>RoboFriends</h1>
-            <img src={`https://robohash.org/${props.id}?200x200`} alt="robots" />
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img src={`https://robohash.org/${id}?200x200`} alt='robots' />
             <div>
-                <h2>{props.name}</h2>
-                <p>{props.email}</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
 }
-
 
 export default Card;
