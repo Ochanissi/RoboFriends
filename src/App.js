632a96be7cd6e54093e3fbdd938068cfb31759a8
Ochanissi <mirelalex2094@gmail.com>
2019-01-21 23:22:56 +0200

    Adding the SearchBox.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import CardList from './CardList';
+import SearchBox from './SearchBox';
 import { robots } from './robots';
 
 const App = () => {
     return (
-        <CardList robots={robots} />
+        <div>
+            <h1>RoboFriends</h1>
+            <SearchBox />
+            <CardList robots={robots} />
+        </div>
+
     );
 }
+
+export default App;
