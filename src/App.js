fce3a85827dcd18b66904380e8212c576dd648e1
Ochanissi <mirelalex2094@gmail.com>
2019-01-22 20:05:50 +0200

    Getting the robots from API instead of hardcoded

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: '',
         }
     }
 
+    componentDidMount() {
+        fetch('http://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({ robots: users}));
+
+    }
+
+
     onSearchChange = (event) => {
         this.setState({searchfield: event.target.value})
-        //console.log(filteredRobots);
     }
 
 
@@ -23,14 +29,18 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        return (
-            <div className='tc'>
-                <h1>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots} />
-            </div>
-    
-        );
+        if (this.state.robots.length === 0) {
+            return <h1> Loading</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList robots={filteredRobots} />
+                </div>
+        
+            );
+        }
     }
 }
 
