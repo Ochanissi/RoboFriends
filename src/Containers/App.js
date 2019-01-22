302835587f8adcf7a479d1a8c46b9d2d172e0926
Ochanissi <mirelalex2094@gmail.com>
2019-01-22 20:59:52 +0200

    Cleaning up the code and finishing the App

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -28,26 +28,23 @@ class App extends Component {
 
 
     render() {
-        const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const { robots, searchfield} = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-        if (this.state.robots.length === 0) {
-            return <h1> Loading</h1>
-        } else {
-            return (
+        return !robots.length ?
+        <h1> Loading</h1> :
+            (
                 <div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
                     <Scroll>
                         <CardList robots={filteredRobots} />
                     </Scroll>
-
                 </div>
-        
             );
         }
     }
-}
 
 
 export default App;
