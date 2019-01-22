cfa09d63560a6a11318af4e9114a02b30aebcc33
Ochanissi <mirelalex2094@gmail.com>
2019-01-22 19:33:56 +0200

    Adding the functionality of the searcbox

--- src/App.js
+++ src/App.js
@@ -3,23 +3,36 @@ import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-const state = {
-    robots: robots,
-    searchfield: ''
-}
 
 class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: '',
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+        //console.log(filteredRobots);
+    }
+
+
     render() {
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
         return (
             <div className='tc'>
                 <h1>RoboFriends</h1>
-                <SearchBox />
-                <CardList robots={robots} />
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={filteredRobots} />
             </div>
     
         );
     }
-    }
+}
 
 
 export default App;
