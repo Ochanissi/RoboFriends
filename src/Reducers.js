4ecc335697f5c7fd367eace5e497baf5f9203113
Ochanissi <mirelalex2094@gmail.com>
2019-01-23 19:59:50 +0200

    Started using React-Redux with the Provider

--- src/Reducers.js
+++ src/Reducers.js
@@ -6,7 +6,7 @@ const initialState = {
 
 export const searchRobots = (state=initialState, action={}) => {
     switch(action.type) {
-        case: CHANGE_SEARCH_FIELD: 
+        case CHANGE_SEARCH_FIELD: 
             return Object.assign({}, state, {searchField:action.payload});
         default:
             return state;
